938c9dc07bf0f0a3c250f2824b80acab0349c354
Rafal <rafal.wilmanowicz@gmail.com>
2018-03-23 14:19:37 +0100

    Hello Swarm

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
 
     // Send the response body as "Hello World"
-    response.end('Hello World\n');
+    response.end('Hello Swarm\n');
 }).listen(80);
